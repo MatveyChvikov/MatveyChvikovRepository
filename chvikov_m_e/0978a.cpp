99e0f7cc98323e29d83b3201f81cc7471da7a562
MatveyChvikov <114420293+MatveyChvikov@users.noreply.github.com>
2022-11-02 22:06:41 +0300

    Add files via upload

---
+++ chvikov_m_e/0978a.cpp
@@ -0,0 +1,38 @@
+﻿
+#include <iostream>
+#include <array>
+using namespace std;
+
+int main()
+{
+	int n; int k = 0;
+	cin >> n;
+	const int nmax = 50;
+	array <int, nmax> a{ 0 };
+	for (int i = 0; i < n; i++) cin >> a[i];
+
+	for (int i = n - 1; i >= 0; i--)
+		for (int j = i - 1; j >= 0; j--)
+			if (a[j] == a[i])
+			{
+				a[j] = 0;
+			}
+
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] != 0)
+		{
+			k = k + 1;
+		}
+	}
+	cout << k << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] != 0)
+		{
+			cout << a[i] << " ";
+		}
+	}
+	return 0;
+}
