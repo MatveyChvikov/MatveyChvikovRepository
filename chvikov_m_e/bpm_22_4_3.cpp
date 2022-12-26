ce0333e206440e920ba9808f078db117053d346d
MatveyChvikov <114420293+MatveyChvikov@users.noreply.github.com>
2022-12-26 21:17:53 +0300

    Update bpm_22_4_3.cpp

--- chvikov_m_e/bpm_22_4_3.cpp
+++ chvikov_m_e/bpm_22_4_3.cpp
@@ -1,7 +1,7 @@
 ﻿
 #include <iostream>
 #include <string>
-#include <array>
+#include <cmath>
 
 int main()
 {
@@ -9,7 +9,47 @@ int main()
 	std::string b;
 	std::cin >> a;
 	std::cin >> b;
-	
+	int count = 0;
+	int diff = 0;
+	if (a == b)
+		std::cout << 0;
+	else if (a != b)
+	{
+
+		diff = a.length() - b.length();
+		diff = abs(diff);
+		if (a.length() > b.length())
+		{
+			for (int i = 0; i < diff; i++)
+			{
+				a.erase(0, 1);
+				count++;
+			}
+		}
+		else if (a.length() < b.length())
+		{
+			for (int i = 0; i < diff; i++)
+			{
+				b.erase(0, 1);
+				count++;
+			}
+		}
+		if (a == b)
+			std::cout << count;
+		else if (a != b)
+		{
+			for (int i = 0; i < a.length(); i++)
+			{
+				a.erase(0, 1);
+				b.erase(0, 1);
+				count += 2;
+				if (a == b)
+					break;
+			}
+
+		}
+		std::cout << count;
+	}
 }
 
 
