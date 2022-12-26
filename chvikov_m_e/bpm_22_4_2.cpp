a953576c86a55c7204891082b983def1d96efbe2
MatveyChvikov <114420293+MatveyChvikov@users.noreply.github.com>
2022-12-27 00:06:14 +0300

    Update bpm_22_4_2.cpp

--- chvikov_m_e/bpm_22_4_2.cpp
+++ chvikov_m_e/bpm_22_4_2.cpp
@@ -1,10 +1,20 @@
 ﻿
 #include <iostream>
 
-
 int main()
 {
-	int x = 0; int y = 0;
+	double x = 0, y = 0;
+	int count = 0, oblcount = 0;
+	int n = 0;
+	std::cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		std::cin >> x >> y;
+		count += 1;
+		if (x * x + y * y <= 1 && (y >= x + 1 || y <= x - 1))
+			oblcount += 1;
+	}
+	std::cout << 1.0 * oblcount / count; //1.0 - вещественное умножение
 }
 
 
